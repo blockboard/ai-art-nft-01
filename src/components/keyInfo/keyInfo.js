14aa238f199ee8ac2f2b7d9dd11820ebaebf672e
iskanderandrews <iskander.s.andrews@gmail.com>
2022-04-22 20:10:32 +0200

    fix: fixing releasisng time

--- src/components/keyInfo/keyInfo.js
+++ src/components/keyInfo/keyInfo.js
@@ -7,7 +7,7 @@ function keyInfo() {
       <div className="key-info">
         <h1 className="key-info-header">Key information</h1>
         <p className="key-info-paragraph">
-          Release Date: 11th April 2022 <br />
+          Release Date: 22<sup>nd</sup> April 2022 <br />
           Total number of tokens: 100 <br />
           Price per token: 0.2 ETH <br />
           Token type: ERC-721 <br />
